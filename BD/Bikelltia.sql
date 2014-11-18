2f37958192048a99909a8e9c78901217a56e905b
dievacri <diego_vas_martin@hotmail.com>
2014-11-18 12:00:42 -0500

    BD

--- BD/Bikelltia.sql
+++ BD/Bikelltia.sql
@@ -1,16 +1,17 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-CREATE SCHEMA IF NOT EXISTS `Bikelltia` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `Bikelltia` ;
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+CREATE SCHEMA IF NOT EXISTS `Bikelltia` DEFAULT CHARACTER SET utf8 ;
+USE `Bikelltia`;
 
 -- -----------------------------------------------------
 -- Table `Bikelltia`.`perfil`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `Bikelltia`.`perfil` (
-  `idPerfil` INT NOT NULL ,
-  `descripcion` VARCHAR(100) NULL ,
+  `idPerfil` INT NOT NULL AUTO_INCREMENT ,
+  `descripcion` VARCHAR(100) NULL DEFAULT NULL ,
   `estadoPerfil` BIT NULL DEFAULT 1 ,
   PRIMARY KEY (`idPerfil`) )
 ENGINE = InnoDB;
@@ -20,9 +21,9 @@ ENGINE = InnoDB;
 -- Table `Bikelltia`.`usuario`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `Bikelltia`.`usuario` (
-  `idUsuario` INT NOT NULL ,
-  `nombreUsuario` VARCHAR(100) NULL ,
-  `contrasena` VARCHAR(100) NULL ,
+  `idUsuario` INT NOT NULL AUTO_INCREMENT ,
+  `nombreUsuario` VARCHAR(100) NULL DEFAULT NULL ,
+  `contrasena` VARCHAR(100) NULL DEFAULT NULL ,
   `estadoUsuario` BIT NULL DEFAULT 1 ,
   `idPerfil` INT NOT NULL ,
   PRIMARY KEY (`idUsuario`) ,
@@ -39,8 +40,8 @@ ENGINE = InnoDB;
 -- Table `Bikelltia`.`permisos`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `Bikelltia`.`permisos` (
-  `idPermiso` INT NOT NULL ,
-  `descripcion` VARCHAR(100) NULL ,
+  `idPermiso` INT NOT NULL AUTO_INCREMENT ,
+  `descripcion` VARCHAR(100) NULL DEFAULT NULL ,
   `estadoPermiso` BIT NULL DEFAULT 1 ,
   PRIMARY KEY (`idPermiso`) )
 ENGINE = InnoDB;
@@ -52,7 +53,7 @@ ENGINE = InnoDB;
 CREATE  TABLE IF NOT EXISTS `Bikelltia`.`perfilPermiso` (
   `idPermiso` INT NOT NULL ,
   `idPerfil` INT NOT NULL ,
-  `descripcion` VARCHAR(100) NULL ,
+  `descripcion` VARCHAR(100) NULL DEFAULT NULL ,
   `estadoPerfilPermiso` BIT NULL DEFAULT 1 ,
   PRIMARY KEY (`idPermiso`, `idPerfil`) ,
   INDEX `fk_perfilPermiso_permisos1_idx` (`idPermiso` ASC) ,
@@ -75,12 +76,12 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `Bikelltia`.`persona` (
   `idPersona` INT NOT NULL ,
-  `nombre` VARCHAR(45) NULL ,
-  `apellidoPaterno` VARCHAR(45) NULL ,
-  `apellidoMaterno` VARCHAR(45) NULL ,
-  `celular` INT NULL ,
-  `telefono` INT NULL ,
-  `direccion` VARCHAR(200) NULL ,
+  `nombre` VARCHAR(45) NULL DEFAULT NULL ,
+  `apellidoPaterno` VARCHAR(45) NULL DEFAULT NULL ,
+  `apellidoMaterno` VARCHAR(45) NULL DEFAULT NULL ,
+  `celular` INT NULL DEFAULT NULL ,
+  `telefono` INT NULL DEFAULT NULL ,
+  `direccion` VARCHAR(200) NULL DEFAULT NULL ,
   `idUsuario` INT NOT NULL ,
   PRIMARY KEY (`idPersona`) ,
   INDEX `fk_persona_usuario_idx` (`idUsuario` ASC) ,
@@ -96,10 +97,10 @@ ENGINE = InnoDB;
 -- Table `Bikelltia`.`orden`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `Bikelltia`.`orden` (
-  `idOrden` INT NOT NULL ,
-  `fecha` DATETIME NULL ,
-  `monto` INT NULL ,
-  `tipoPago` CHAR NULL ,
+  `idOrden` INT NOT NULL AUTO_INCREMENT ,
+  `fecha` DATETIME NULL DEFAULT NULL ,
+  `monto` INT NULL DEFAULT NULL ,
+  `tipoPago` CHAR NULL DEFAULT NULL ,
   `idPersona` INT NOT NULL ,
   PRIMARY KEY (`idOrden`) ,
   INDEX `fk_orden_persona1_idx` (`idPersona` ASC) ,
@@ -115,8 +116,8 @@ ENGINE = InnoDB;
 -- Table `Bikelltia`.`tipoProducto`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `Bikelltia`.`tipoProducto` (
-  `idTipoProducto` INT NOT NULL ,
-  `descripcion` VARCHAR(100) NULL ,
+  `idTipoProducto` INT NOT NULL AUTO_INCREMENT ,
+  `descripcion` VARCHAR(100) NULL DEFAULT NULL ,
   `estadoTipoProducto` BIT NULL DEFAULT 1 ,
   PRIMARY KEY (`idTipoProducto`) )
 ENGINE = InnoDB;
@@ -126,10 +127,10 @@ ENGINE = InnoDB;
 -- Table `Bikelltia`.`producto`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `Bikelltia`.`producto` (
-  `idProducto` INT NOT NULL ,
-  `nombre` VARCHAR(100) NULL ,
-  `precio` INT NULL ,
-  `descripcion` VARCHAR(100) NULL ,
+  `idProducto` INT NOT NULL AUTO_INCREMENT ,
+  `nombre` VARCHAR(100) NULL DEFAULT NULL ,
+  `precio` INT NULL DEFAULT NULL ,
+  `descripcion` VARCHAR(100) NULL DEFAULT NULL ,
   `estadoProducto` BIT NULL DEFAULT 1 ,
   `idTipoProducto` INT NOT NULL ,
   PRIMARY KEY (`idProducto`) ,
@@ -148,7 +149,7 @@ ENGINE = InnoDB;
 CREATE  TABLE IF NOT EXISTS `Bikelltia`.`detalleOrden` (
   `idOrden` INT NOT NULL ,
   `idProducto` INT NOT NULL ,
-  `cantidad` INT NULL ,
+  `cantidad` INT NULL DEFAULT NULL ,
   PRIMARY KEY (`idOrden`, `idProducto`) ,
   INDEX `fk_detalleOrden_orden1_idx` (`idOrden` ASC) ,
   INDEX `fk_detalleOrden_producto1_idx` (`idProducto` ASC) ,
@@ -169,12 +170,12 @@ ENGINE = InnoDB;
 -- Table `Bikelltia`.`ocasion`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `Bikelltia`.`ocasion` (
-  `idOcasion` INT NOT NULL ,
-  `descripcion` VARCHAR(100) NULL ,
+  `idOcasion` INT NOT NULL AUTO_INCREMENT ,
+  `descripcion` VARCHAR(100) NULL DEFAULT NULL ,
   `estadoOcasion` BIT NULL DEFAULT 1 ,
-  `fechaInicio` DATE NULL ,
-  `fechaFin` DATE NULL ,
-  `nombreOcasion` VARCHAR(45) NULL ,
+  `fechaInicio` DATE NULL DEFAULT NULL ,
+  `fechaFin` DATE NULL DEFAULT NULL ,
+  `nombreOcasion` VARCHAR(45) NULL DEFAULT NULL ,
   PRIMARY KEY (`idOcasion`) )
 ENGINE = InnoDB;
 
@@ -183,9 +184,9 @@ ENGINE = InnoDB;
 -- Table `Bikelltia`.`promocion`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `Bikelltia`.`promocion` (
-  `idPromocion` INT NOT NULL ,
-  `precio` INT NULL ,
-  `nombrePromocion` VARCHAR(100) NULL ,
+  `idPromocion` INT NOT NULL AUTO_INCREMENT ,
+  `precio` INT NULL DEFAULT NULL ,
+  `nombrePromocion` VARCHAR(100) NULL DEFAULT NULL ,
   `estadoPromocion` BIT NULL DEFAULT 1 ,
   `idOcasion` INT NOT NULL ,
   PRIMARY KEY (`idPromocion`) ,
@@ -204,7 +205,7 @@ ENGINE = InnoDB;
 CREATE  TABLE IF NOT EXISTS `Bikelltia`.`productoPromocion` (
   `idProducto` INT NOT NULL ,
   `idPromocion` INT NOT NULL ,
-  `descripcion` VARCHAR(100) NULL ,
+  `descripcion` VARCHAR(100) NULL DEFAULT NULL ,
   `estadoProductoPromocion` BIT NULL DEFAULT 1 ,
   PRIMARY KEY (`idProducto`, `idPromocion`) ,
   INDEX `fk_productoPromocion_promocion1_idx` (`idPromocion` ASC) ,
@@ -225,8 +226,8 @@ ENGINE = InnoDB;
 -- Table `Bikelltia`.`multimedia`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `Bikelltia`.`multimedia` (
-  `idMultimedia` INT NOT NULL ,
-  `ruta` VARCHAR(300) NULL ,
+  `idMultimedia` INT NOT NULL AUTO_INCREMENT ,
+  `ruta` VARCHAR(300) NULL DEFAULT NULL ,
   `idProducto` INT NOT NULL ,
   PRIMARY KEY (`idMultimedia`) ,
   INDEX `fk_multimedia_producto1_idx` (`idProducto` ASC) ,
@@ -237,13 +238,11 @@ CREATE  TABLE IF NOT EXISTS `Bikelltia`.`multimedia` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-USE `Bikelltia` ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
 
 insert into permisos values(1,'ver Gestionar Producto',1);
 insert into permisos values(2,'ver Gestionar Promocion',1);
